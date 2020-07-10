754b688af1e0fc05af4085097ce5b4ae56ff9cc9
Jason Maurer <jason@maur.co>
2020-07-09 19:38:22 -0600

    Code from guided lecture

--- middleware/restrict.js
+++ middleware/restrict.js
@@ -1,17 +1,39 @@
-function restrict() {
+const jwt = require("jsonwebtoken")
+
+// use a scale for checking user roles since admin users
+// should still have access to normal user endpoints,
+// but normal users shouldn't have access to admin endpoints.
+const roles = [
+	"normal",
+	"admin",
+]
+
+function restrict(role) {
 	return async (req, res, next) => {
 		const authError = {
 			message: "Invalid credentials",
 		}
 
 		try {
-			// express-session will automatically get the session ID from the cookie
-			// header, and check to make sure it's valid and the session for this user exists.
-			if (!req.session || !req.session.user) {
+			// manually pull the token that got sent from the client's cookie jar
+			const token = req.cookies.token
+			if (!token) {
 				return res.status(401).json(authError)
 			}
 
-			next()
+			// checks to make sure the signature is valid and the token is not expired
+			jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+				if (err) {
+					return res.status(401).json(authError)
+				}
+
+				// check if the role in our token is above or equal to the required role for the endpoint
+				if (role && roles.indexOf(decoded.userRole) < roles.indexOf(role)) {
+					return res.status(401).json(authError)
+				}
+
+				next()
+			})
 		} catch(err) {
 			next(err)
 		}
--- users/users-router.js
+++ users/users-router.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const Users = require("./users-model")
 const restrict = require("../middleware/restrict")
 
 const router = express.Router()
 
-router.get("/users", restrict(), async (req, res, next) => {
+router.get("/users", restrict("normal"), async (req, res, next) => {
 	try {
 		res.json(await Users.find())
 	} catch(err) {
@@ -56,31 +57,19 @@ router.post("/login", async (req, res, next) => {
 			})
 		}
 
-		// generate a new session for this user,
-		// and sends back a session ID
-		req.session.user = user
+		const payload = {
+			userId: user.id,
+			username: user.username,
+			userRole: "admin", // this value would usually come from the database
+		}
+
+		// send the token back as a cookie so the client automatically stores it
+		res.cookie("token", jwt.sign(payload, process.env.JWT_SECRET))
 
 		res.json({
 			message: `Welcome ${user.username}!`,
 		})
 	} catch(err) {
-		next(err)
-	}
-})
-
-router.get("/logout", async (req, res, next) => {
-	try {
-		// this will delete the session in the database and try to expire the cookie,
-		// though it's ultimately up to the client if they delete the cookie or not.
-		// but it becomes useless to them once the session is deleted server-side.
-		req.session.destroy((err) => {
-			if (err) {
-				next(err)
-			} else {
-				res.status(204).end()
-			}
-		})
-	} catch (err) {
 		next(err)
 	}
 })
